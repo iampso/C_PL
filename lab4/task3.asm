a2704f900b57835a39179ea736123886c05d9eac
roman <116097799+iampso@users.noreply.github.com>
2024-10-02 18:29:01 +0500

    1

---
+++ lab4/task3.asm
@@ -0,0 +1,189 @@
+.ORIG x3000
+
+LEA R0, FIRSTPROMPT
+PUTS
+
+LEA R0, SPACE
+PUTS
+
+LEA R0, SAMPLE
+PUTS
+
+LD R3, POINTER
+LD R6, IOCOUNTER
+
+INPUT
+    IN
+    AND R2, R2, x0 
+    AND R5, R5, x0 
+    LD R5, HUNDRED
+    LD R2, HEXN48
+    ADD R0, R0, R2
+    ADD R2, R0, x0
+    ADD R0, R0, x0
+
+FIRST_NUM
+    ADD R0, R0, R2
+    ADD R5, R5, x-1
+    BRp FIRST_NUM
+    ADD R1, R0, x0
+    AND R0, R0, x0
+
+IN
+AND R2, R2, x0
+AND R5, R5, x0
+LD R5, TEN
+LD R2, HEXN48
+ADD R0, R0, R2
+ADD R2, R0, x0
+AND R0, R0, x0
+
+SECOND_NUM
+    ADD R0, R0, R2
+    ADD R5, R5, x-1
+    BRp SECOND_NUM
+    ADD R4, R0, x0
+    AND R0, R0, x0
+
+IN
+AND R2, R2, x0
+LD R2, HEXN48
+ADD R0, R0, R2
+AND R2, R2, x0
+
+ADD R2, R1, R4
+ADD R2, R0, R2
+STR R2, R3, x0
+ADD R3, R3, x1
+ADD R6, R6, x-1
+BRp INPUT
+
+JSR BUBBLESORT
+JSR PRODLOOP
+
+HALT
+
+BUBBLESORT
+    AND R3, R3, x0
+    LD R3, POINTER
+    AND R4, R4, x0
+    LD R4, IOCOUNTER
+    AND R5, R5, x0
+    LD R5, IOCOUNTER
+
+OUTLOOP
+    ADD R4, R4, x-1
+    BRz SORTED
+    ADD R5, R4, x0
+    LD R3, POINTER
+
+INLOOP
+    LDR R0, R3, x0
+    LDR R1, R3, x1
+    AND R2, R2, x0
+    NOT R2, R1
+    ADD R2, R2, x1
+    ADD R2, R0, R2
+    BRn AUTOSWAP
+
+    STR R1, R3, x0
+    STR R0, R3, x1
+
+AUTOSWAP
+    ADD R3, R3, x1
+    ADD R5, R5, x-1
+    BRp INLOOP
+    BRzp OUTLOOP
+SORTED RET
+RET
+
+PRODLOOP
+    LEA R0, PROMPTEX
+    PUTS
+    LD R3, POINTER
+    LD R6, IOCOUNTER
+
+RESLOOP
+    AND R1, R1, x0
+    AND R2, R2, x0
+    AND R4, R4, x0
+    AND R5, R5, x0
+    AND R0, R0, x0
+    LD R0, SPACE
+    OUT
+    AND R0, R0, x0
+    LDR R0, R3, x0
+
+    LD R2, HUNDRED
+    NOT R2, R2
+    ADD R2, R2, x1
+
+    MINUS1
+        ADD R1, R1, x1
+        ADD R0, R0, R2
+        BRzp MINUS1
+
+    REMAINDER1
+        AND R2, R2, x0
+        LD R2, HUNDRED
+        ADD R0, R0, R2
+        ADD R1, R1, x-1
+        STI R1, FIRSTNUM
+
+    AND R2, R2, x0
+    LD R2, TEN
+    NOT R2, R2
+    ADD R2, R2, x1
+
+    MINUS2
+        ADD R4, R4, x1
+        ADD R0, R0, R2
+        BRzp MINUS2
+
+    REMAINDER2
+        AND R2, R2, x0
+        LD R2, TEN
+        ADD R5, R0, R2
+        STI R5, THIRDNUM
+        ADD R4, R4, x-1
+        STI R4, SECONDNUM
+
+    AND R0, R0, x0
+    LDI R0, FIRSTNUM
+    AND R2, R2, x0
+    LD R2, HEX48
+    ADD R0, R0, R2
+    OUT
+    AND R0, R0, x0
+    LDI R0, SECONDNUM
+    AND R2, R2, x0
+    LD R2, HEX48
+    ADD R0, R0, R2
+    OUT
+
+    AND R0, R0, x0
+    LDI R0, THIRDNUM
+    AND R2, R2, x0
+    LD R2, HEX48
+    ADD R0, R0, R2
+    OUT
+
+    ADD R3, R3, x1
+    ADD R6, R6, x-1
+    BRp RESLOOP
+    HALT
+
+FIRSTPROMPT .STRINGZ  "Input 10 num (0-999) with 3 digits"
+SAMPLE .STRINGZ  "Input exaple: 020 - 20, 003 - 3 and etc."
+PROMPTEX .STRINGZ "Numbers in ascending order:"
+SPACE .STRINGZ "\n"
+HEXN48 .FILL  xFFD0
+HEX48 .FILL x0030
+HUNDRED .FILL x0064
+TEN .FILL x000A
+POINTER .FILL  x4000
+IOCOUNTER .FILL  #10
+FIRSTNUM .FILL x400A
+SECONDNUM .FILL x400B
+THIRDNUM .FILL x400C
+.END
